1d264a2e4ce45fa0c20b1d909cf7c86c1505b3a8
Vytautas Butkevicius <vytautas.butkevicius@auriga.com>
2020-12-23 15:06:06 +0200

    use "cargo run" to flash changes to the board.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rust-arduino-blink
+Make sure board is connected and just run `cargo run` to build & flash to Arduino UNO.
--- flash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-avrdude -q -C/etc/avrdude.conf -patmega328p -carduino -P/dev/ttyACM0 -D "-Uflash:w:$1:e"
